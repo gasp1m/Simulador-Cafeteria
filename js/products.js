25e5121deb3a8cf1c4aefaea2c3ad42db8e053a4
theweeknd <gasparmmaraschio@gmail.com>
2022-09-30 19:42:57 -0300

    Proyecto Casi Terminado

--- js/products.js
+++ js/products.js
@@ -4,30 +4,35 @@ const productos = [
         nombre: 'Cappuccino',
         precio: 350,
         img: 'https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-CAPPUCCINO-BANANA-SESAME-SEEDS.jpg',
+        cantidad: 1,
     },
     {
         id: 2,
         nombre: 'Americano',
         precio: 300,
         img: 'http://www.somoselcafe.com.ar/img/novedades/47.jpg',
+        cantidad: 1,
     },
     {
         id: 3,
         nombre: 'Latte',
         precio: 300,
         img: 'https://clubdelcafe.net/wp-content/uploads/2020/05/taza-de-caf%C3%A9-latte.jpg',
+        cantidad: 1,
     },
     {
         id: 4,
         nombre: 'Lagrima',
         precio: 300,
         img: 'http://www.serviccafexpress.com.ar/wp-content/uploads/2017/03/lagrima.jpg',
+        cantidad: 1,
     },
     {
         id: 5,
         nombre: 'Mocha',
         precio: 350,
         img: 'https://img.freepik.com/fotos-gratis/cafe-mocha-quente_1339-2386.jpg?w=1380&t=st=1664489197~exp=1664489797~hmac=f4d32bedb84fb4cc348e2d61a7dc5b4d35bbe9521127eda1b4af6869a51a3d93',
+        cantidad: 1,
     },
 ];
 
