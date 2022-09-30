dc7c70d8d98e635d0058f576e0a8fc36e775d957
theweeknd <gasparmmaraschio@gmail.com>
2022-09-29 23:37:58 -0300

    commiteo

---
+++ js/storage.js
@@ -0,0 +1,10 @@
+const guardarCarrito = (carrito) => {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+};
+
+const obtenerCarrito = () => {
+    const carritoStorage = JSON.parse(localStorage.getItem('carrito'));
+    return carritoStorage;
+};
+
+export { guardarCarrito, obtenerCarrito };
